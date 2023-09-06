3abf03edf4a86792a768e40dc824282fa608d690
Hajra Batool <hamna64gb@gmail.com>
2023-09-06 23:25:57 +0500

    Add files via upload

---
+++  (06_09_2023)program.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    string name="Rehana";
+    int age=20;
+    char section='A';
+    string subject="computer science";
+    int marks=90;
+    float avg=9.60;
+    cout<<"----Students detail----\n";
+    cout<<"name="<<name<<"\n";
+    cout<<"age="<<age<<"\n";
+    cout<<"subject="<<subject<<"\n";
+    cout<<"marks="<<marks<<"\n";
+    cout<<"avg="<<avg<<"\n";
+    return 0;
+}

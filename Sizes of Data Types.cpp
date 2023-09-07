bfbf6cd0649d79753a4290fc51618aba1183b1f3
Hajra Batool <hamna64gb@gmail.com>
2023-09-07 21:28:11 +0500

    Add files via upload

---
+++ Sizes of Data Types.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Size of Data Types
+    cout<< "Sizes of All Data Types" <<endl;
+    cout << "size of int is " <<sizeof (int)<<" bytes " <<endl;
+   cout << "size of float is " <<sizeof (float)<<" bytes " <<endl;
+  cout << "size of char is " <<sizeof (char)<<" bytes " <<endl;
+ cout << "size of double is " <<sizeof (double)<<" bytes " <<endl;
+ cout << "size of string is " <<sizeof (string)<<" bytes " <<endl;
+  return 0;
+}
